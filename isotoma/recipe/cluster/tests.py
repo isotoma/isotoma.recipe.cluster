7303c81e201404df6d4e4434789ea77cdb4738ad
John Carr <john.carr@isotoma.com>
2010-09-28 13:33:24 +0100

    Initial skeleton

---
+++ isotoma/recipe/cluster/tests.py
@@ -0,0 +1,44 @@
+"""Test setup for isotoma.recipe.apache.
+"""
+
+import os, re
+import pkg_resources
+
+import zc.buildout.testing
+
+import unittest
+import zope.testing
+from zope.testing import doctest, renormalizing
+
+
+def setUp(test):
+    zc.buildout.testing.buildoutSetUp(test)
+    zc.buildout.testing.install_develop('isotoma.recipe.cluster', test)
+    zc.buildout.testing.install('zope.testing', test)
+
+
+checker = renormalizing.RENormalizing([
+    zc.buildout.testing.normalize_path,
+    (re.compile('#![^\n]+\n'), ''),
+    (re.compile('-\S+-py\d[.]\d(-\S+)?.egg'),
+     '-pyN.N.egg',
+    ),
+    ])
+
+
+def test_suite():
+    tests = [
+        "doctests/simple-usage.txt",
+        ]
+
+    suites = []
+    for test in tests:
+        suites.append(doctest.DocFileSuite(test,
+            setUp=setUp, tearDown=zc.buildout.testing.buildoutTearDown,
+            optionflags=doctest.ELLIPSIS, checker=checker))
+
+    return unittest.TestSuite(suites)
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+version = '0.0.0'
+
+setup(
+    name = 'isotoma.recipe.cluster',
+    version = version,
+    description = "Buildout recipe for generating cluster start/stop scripts.",
+    url = "http://pypi.python.org/pypi/isotoma.recipe.cluster",
+    long_description = open("README.rst").read() + "\n" + \
+                       open("CHANGES.txt").read(),
+    classifiers = [
+        "Framework :: Buildout",
+        "Intended Audience :: System Administrators",
+        "Operating System :: POSIX",
+        "License :: OSI Approved :: Apache Software License",
+
+    ],
+    keywords = "buildout cluster",
+    author = "John Carr",
+    author_email = "john.carr@isotoma.com",
+    license="Apache Software License",
+    packages = find_packages(exclude=['ez_setup']),
+    package_data = {
+        '': ['README.rst', 'CHANGES.txt'],
+    },
+    namespace_packages = ['isotoma', 'isotoma.recipe'],
+    include_package_data = True,
+    zip_safe = False,
+    install_requires = [
+        'setuptools',
+        'zc.buildout',
+        'isotoma.recipe.gocaptain',
+    ],
+    extras_require=dict(
+        test = ['zope.testing',],
+    ),
+    entry_points = {
+        "zc.buildout": [
+            "default = isotoma.recipe.apache:Apache",
+        ],
+    }
+)
